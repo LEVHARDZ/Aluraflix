dad1a2bbc47cedcf10c52ade1fd6b8846796aee5
LEVHARZ <edwardhernan79@gmail.com>
2024-07-15 00:17:23 -0600

    Mejoras en el comportamiento de los botones para acceder a las diferente paginas

--- src/components/Botones/Botones.jsx
+++ src/components/Botones/Botones.jsx
@@ -2,15 +2,13 @@ import botonesStyles from "./Botones.module.css";
 import { Link } from "react-router-dom";
 
 const Botones = (props) => {
-
-  const { ContentText, pagina, estado, onClick } = props
-
+  const { ContentText, pagina, estado } = props;
 
   return (
     <Link to={pagina}>
       <button
-        onClick={onClick}
         className={estado ? botonesStyles.activeBtn : botonesStyles.noActiveBtn}
+        disabled={estado}
       >
         {ContentText}
       </button>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,32 @@
-import headerStyles from "./Header.module.css"
+import headerStyles from "./Header.module.css";
 import Botones from "../Botones/Botones";
 import { useState } from "react";
-
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const paginaHome = "/";
+  const paginaNuevoVideo = "/nuevo-video";
+  let rutaActual = useLocation().pathname;
 
-  const [estadoBtn1, setEstadoBtn1] = useState(true);
-  const [estadoBtn2, setEstadoBtn2] = useState(false);
-
-  const handleBtn1Click = () => {
-    setEstadoBtn1(true);
-    setEstadoBtn2(false);
-  };
-
-  const handleBtn2Click = () => {
-    setEstadoBtn1(false);
-    setEstadoBtn2(true);
-  };
+  const estadoHome = rutaActual == paginaHome ? true : false;
+  const estadoNuevoVideo = rutaActual == paginaNuevoVideo ? true : false;
 
   return (
-      <header className={headerStyles.LogoHeader}>
-        <img src="img\aluraflix-logo.svg" alt="aluraflix" />
-        <div className={headerStyles.contentBtns}>
-          <Botones ContentText="HOME" pagina="/" estado={estadoBtn1} onClick={handleBtn1Click} />
-          <Botones ContentText="NUEVO VIDEO" pagina="/nuevo-video" estado={estadoBtn2} onClick={handleBtn2Click} />
-        </div>
-      </header>
+    <header className={headerStyles.LogoHeader}>
+      <img src="img\aluraflix-logo.svg" alt="aluraflix" />
+      <div className={headerStyles.contentBtns}>
+        <Botones
+          ContentText="HOME"
+          pagina={paginaHome}
+          estado={estadoHome}
+        />
+        <Botones
+          ContentText="NUEVO VIDEO"
+          pagina={paginaNuevoVideo}
+          estado={estadoNuevoVideo}
+        />
+      </div>
+    </header>
   );
 };
 
